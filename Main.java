44a7a57c9ef719211b886155cf14223ee4a55328
Nandini Bulusu <nanbul785@gmail.com>
2019-05-22 15:11:17 -0700

    Added basic functionality to chat

--- Main.java
+++ Main.java
@@ -1,5 +1,7 @@
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 
 public class Main extends JFrame {
@@ -7,7 +9,7 @@ public class Main extends JFrame {
     public Main() {
 
         //Setting up the Frame
-        super("First Frame");
+        super("Chat Application");
         setSize(500, 300);
         setLocation(400, 300);
 
@@ -20,21 +22,50 @@ public class Main extends JFrame {
         jm1.add(new JMenuItem("Open"));
         jm1.add(new JMenuItem("Save as"));
 
+        // Text Area
+        final JTextArea textArea = new JTextArea();
+
+        //Make sure you can't edit the actual chat box
+        textArea.setEditable(false);
+
         //Bottom Panel
         JPanel jPanel = new JPanel();
         JLabel label = new JLabel("Enter Text");
-        JTextField textField = new JTextField(10);
+        final JTextField textField = new JTextField(10);
         JButton button1 = new JButton("Send");
+
+        // Method for sending a message
+        // An Action Listener listens for action (i.e. if it's clicked)
+        button1.addActionListener(new ActionListener() {
+                                      @Override
+                                      // This method performs an action once the button is clicked
+                                      public void actionPerformed(ActionEvent e) {
+                                          // We add the text from the text field in one line
+                                          textArea.append(textField.getText() + "\n");
+                                          // We erase the text field to add something new
+                                          textField.setText("");
+                                      }
+                                  });
+
+        // Method for resetting the chat box
         JButton button2 = new JButton("Reset");
+        button2.addActionListener(new ActionListener() {
+                                      @Override
+                                      public void actionPerformed(ActionEvent e) {
+                                          // Set the text area to blank
+                                          textArea.setText("");
+                                      }
+                                  });
+
+        // Adding all the elements to the panel
         jPanel.add(label);
         jPanel.add(textField);
         jPanel.add(button1);
         jPanel.add(button2);
 
-        // Text Area
-        JTextArea textArea = new JTextArea();
 
-        // P
+
+        // Adding the various areas to the overall container
         Container cp = getContentPane();
         cp.add(BorderLayout.SOUTH, jPanel);
         cp.add(BorderLayout.NORTH, jmb);
